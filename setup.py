14dc0a87f29b29e67d0be5c039c41e8a72fae8e1
rahulgauli <rahul.gauli1997@gmail.com>
2023-10-04 22:49:59 -0500

    Second commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Metadata store awesome'
+LONG_DESCRIPTION = 'A package that allows to Store and view metadata in awesome style'
+
+# Setting up
+setup(
+    name="datameta",
+    version=VERSION,
+    author="Pnemenox Rahul Gauli",
+    author_email="rahul.gauli13@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pydantic', 'pendulum'],
+    keywords=['python', 'meta', 'data'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
